c1c66a720c87f362979037be3740a0b460a456f7
ryuichi-works <ryu1.works@gmail.com>
2023-11-10 17:44:22 +0000

    makerモデル、マイグレーションファイルの作成

---
+++ database/migrations/2023_11_10_173843_create_makers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('makers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_ja', 30)->nullable(false);
+            $table->string('name_en', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('makers');
+    }
+};
