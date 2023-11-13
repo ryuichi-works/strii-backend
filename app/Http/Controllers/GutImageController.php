c4afa9b70855185d3399402f3a4eabfdc9742407
ryuichi-works <ryu1.works@gmail.com>
2023-11-13 12:55:23 +0000

    gut画像登録機能の実装storeメソッド
    storageとdb両方に情報を保存されるようにした

--- app/Http/Controllers/GutImageController.php
+++ app/Http/Controllers/GutImageController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\GutImage;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Requests\GutImage\GutImageStoreRequest;
 
 class GutImageController extends Controller
 {
@@ -21,7 +22,7 @@ class GutImageController extends Controller
             $gut_images = GutImage::all();
 
             return response()->json($gut_images, 200);
-        } catch(\Throwable $e) {
+        } catch (\Throwable $e) {
             \Log::error($e);
 
             return $e;
@@ -34,9 +35,31 @@ class GutImageController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    // public function store(Request $request)
+    public function store(GutImageStoreRequest $request)
     {
-        
+        $validated_request = $request->validated();
+
+        try {
+            $file = $request->file('file');
+
+            $filename = now()->format('YmdHis') . $validated_request['title'] . "." . $request->file('file')->extension();
+
+            $path = $file->storeAs('images/guts', $filename, 'public');
+
+            $gutImage = GutImage::create([
+                'file_path' => Storage::url($path),
+                'title' => $validated_request['title']
+            ]);
+
+            if(isset($gutImage)) {
+                return response()->json($gutImage, 200);
+            }
+        } catch (\Throwable $e) {
+            \Log::error($e);
+
+            return $e;
+        }
     }
 
     /**
