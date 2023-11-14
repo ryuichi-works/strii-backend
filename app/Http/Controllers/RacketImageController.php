dd03f30e6d33c685579ea9d585126c9e8b56dd5c
ryuichi-works <ryu1.works@gmail.com>
2023-11-14 14:57:30 +0000

    racket_image登録apiの実装storeメソッド
    dbにファイル名、Storageにファイルの実態を保存

--- app/Http/Controllers/RacketImageController.php
+++ app/Http/Controllers/RacketImageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RacketImage\RacketImageStoreRequest;
 use App\Models\RacketImage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -45,9 +46,33 @@ class RacketImageController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(RacketImageStoreRequest $request)
     {
-        //
+        $validated_request = $request->validated();
+
+        try {
+            $file = $request->file('file');
+
+            $filename = now()->format('YmdHis') . $validated_request['title'] . "." . $request->file('file')->extension();
+
+            $path = $file->storeAs('images/rackets', $filename, 'public');
+
+            $racket_image = RacketImage::create([
+                'file_path' => $path,
+                'title' => $validated_request['title']
+            ]);
+
+            if (isset($racket_image)) {
+                return response()->json([
+                    'file_path' => Storage::url($racket_image['file_path']),
+                    'title' => $racket_image['title']
+                ], 200);
+            }
+        } catch (\Throwable $e) {
+            \Log::error($e);
+
+            return $e;
+        }
     }
 
     /**
--- app/Models/RacketImage.php
+++ app/Models/RacketImage.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class RacketImage extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'file_path',
+        'title'
+    ];
 }
