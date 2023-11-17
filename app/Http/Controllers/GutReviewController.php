177ed7e0abe64c783ee7b3157822e595f9235ebc
ryuichi-works <ryu1.works@gmail.com>
2023-11-17 15:50:10 +0000

    add: gutReview一覧情報取得apiの実装indexメソッド
    withメソッドでネストしたリレーションの情報も取得した。

--- app/Http/Controllers/GutReviewController.php
+++ app/Http/Controllers/GutReviewController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\GutReview;
 
 class GutReviewController extends Controller
 {
@@ -12,8 +13,28 @@ class GutReviewController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        //
+    {   
+        try {
+            $gut_reviews = GutReview::with([
+                'myEquipment' => [
+                    'user:id,name',
+                    'mainGut:id,name_ja,name_en,need_posting_image,maker_id,image_id' => [
+                        'maker:id,name_ja,name_en',
+                        'gutImages:id,file_path,title'
+                    ],
+                    'crossGut:id,name_ja,name_en,need_posting_image,maker_id,image_id' => [
+                        'maker:id,name_ja,name_en',
+                        'gutImages:id,file_path,title'
+                    ],
+                ]
+            ])->get();
+    
+            return response()->json($gut_reviews, 200);
+        } catch (\Throwable $e) {
+            \Log::error($e);
+
+            throw $e;
+        }
     }
 
     /**
