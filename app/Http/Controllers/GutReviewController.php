a6f73e80932b8bd278b3fb4e0772fab438314609
ryuichi-works <ryu1.works@gmail.com>
2023-11-27 18:25:42 +0000

    modify: gutReviewControllerで使っているwithメソッドのeager loading対象のカラムの変更
    カラムを絞っていたがフロントで扱いやすくするため絞らずにeager loadingさせた

--- app/Http/Controllers/GutReviewController.php
+++ app/Http/Controllers/GutReviewController.php
@@ -15,22 +15,30 @@ class GutReviewController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {   
+    {
         try {
             $gut_reviews = GutReview::with([
                 'myEquipment' => [
-                    'user:id,name',
-                    'mainGut:id,name_ja,name_en,need_posting_image,maker_id,image_id' => [
-                        'maker:id,name_ja,name_en',
-                        'gutImage:id,file_path,title'
-                    ],
-                    'crossGut:id,name_ja,name_en,need_posting_image,maker_id,image_id' => [
-                        'maker:id,name_ja,name_en',
-                        'gutImage:id,file_path,title'
-                    ],
+                    'user',
+                    'racket' => ['maker', 'racketImage'],
+                    'mainGut' => ['maker', 'gutImage'],
+                    'crossGut' => ['maker', 'gutImage'],
                 ]
             ])->get();
-    
+            // $gut_reviews = GutReview::with([
+            //     'myEquipment' => [
+            //         'user:id,name',
+            //         'mainGut:id,name_ja,name_en,need_posting_image,maker_id,image_id' => [
+            //             'maker:id,name_ja,name_en',
+            //             'gutImage:id,file_path,title'
+            //         ],
+            //         'crossGut:id,name_ja,name_en,need_posting_image,maker_id,image_id' => [
+            //             'maker:id,name_ja,name_en',
+            //             'gutImage:id,file_path,title'
+            //         ],
+            //     ]
+            // ])->get();
+
             return response()->json($gut_reviews, 200);
         } catch (\Throwable $e) {
             \Log::error($e);
@@ -48,7 +56,7 @@ class GutReviewController extends Controller
     public function store(GutReviewStoreRequest $request)
     {
         $validated = $request->validated();
-        
+
         try {
             $gut_review = GutReview::create([
                 'equipment_id' => $validated['equipment_id'],
@@ -57,11 +65,11 @@ class GutReviewController extends Controller
                 'performance_durability' => $validated['performance_durability'],
                 'review' => empty($validated['review']) ? '' : $validated['review']
             ]);
-    
-            if($gut_review) {
+
+            if ($gut_review) {
                 return response()->json('ガットレビューを投稿しました', 200);
             }
-        } catch(\Throwable $e) {
+        } catch (\Throwable $e) {
             \Log::error($e);
 
             throw $e;
@@ -79,22 +87,17 @@ class GutReviewController extends Controller
         try {
             $gut_review = GutReview::with([
                 'myEquipment' => [
-                    'user:id,name',
-                    'mainGut:id,name_ja,name_en,need_posting_image,maker_id,image_id' => [
-                        'maker:id,name_ja,name_en',
-                        'gutImage:id,file_path,title'
-                    ],
-                    'crossGut:id,name_ja,name_en,need_posting_image,maker_id,image_id' => [
-                        'maker:id,name_ja,name_en',
-                        'gutImage:id,file_path,title'
-                    ],
+                    'user',
+                    'racket' => ['maker', 'racketImage'],
+                    'mainGut' => ['maker', 'gutImage'],
+                    'crossGut' => ['maker', 'gutImage'],
                 ]
             ])->findOrFail($id);
-    
+
             return response()->json($gut_review, 200);
-        } catch(\ModelNotFoundException $e) {
+        } catch (\ModelNotFoundException $e) {
             throw $e;
-        } catch(\Throwable $e) {
+        } catch (\Throwable $e) {
             \Log::error($e);
 
             throw $e;
@@ -114,15 +117,15 @@ class GutReviewController extends Controller
 
         try {
             $gut_review = GutReview::findOrFail($id);
-    
+
             $gut_review->match_rate = $validated['match_rate'];
             $gut_review->pysical_durability = $validated['pysical_durability'];
             $gut_review->performance_durability = $validated['performance_durability'];
             $gut_review->review = empty($validated['review']) ? '' : $validated['review'];
-    
-            if($gut_review->save()) {
+
+            if ($gut_review->save()) {
                 return response()->json('ガットレビューを更新しました', 200);
-            } 
+            }
         } catch (\ModelNotFoundException $e) {
             throw $e;
         } catch (\Throwable $e) {
