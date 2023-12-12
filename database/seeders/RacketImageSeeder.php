121ddc6a742ac55d44688998ab4409ef864ec58f
ryuichi-works <ryu1.works@gmail.com>
2023-12-12 18:43:55 +0000

    modify:　RacketImageSeederでmaker_idカラムに関して追加修正とMakerSeederにその他のメーカーを追加
    racket_imagesテーブルにmaker_idを追加したことによる調整のために修正

--- database/seeders/RacketImageSeeder.php
+++ database/seeders/RacketImageSeeder.php
@@ -18,14 +18,58 @@ class RacketImageSeeder extends Seeder
     {
         DB::table('racket_images')->insert([
             [
-                'file_path' => 'images/rackets/sample_racket_image1.png',
-                'title' => 'プロスタッフ',
+                'file_path' => 'images/rackets/default_racket_image.png',
+                'title' => 'デフォルトラケットイメージ',
+                'maker_id' => 1,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
             [
-                'file_path' => 'images/rackets/sample_racket_image2.jpg',
+                'file_path' => 'images/rackets/default_racket_image1.jpg',
                 'title' => 'ピュアアエロ',
+                'maker_id' => 3,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'file_path' => 'images/rackets/default_racket_image2.jpg',
+                'title' => 'ブレード',
+                'maker_id' => 4,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'file_path' => 'images/rackets/default_racket_image3.jpg',
+                'title' => 'プロスタッフ',
+                'maker_id' => 4,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'file_path' => 'images/rackets/default_racket_image4.jpg',
+                'title' => 'v コア100',
+                'maker_id' => 2,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'file_path' => 'images/rackets/default_racket_image5.jpg',
+                'title' => 'スピード',
+                'maker_id' => 5,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'file_path' => 'images/rackets/default_racket_image6.jpg',
+                'title' => 'o3',
+                'maker_id' => 6,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'file_path' => 'images/rackets/default_racket_image7.jpg',
+                'title' => 'ピュアドライブ',
+                'maker_id' => 3,
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
