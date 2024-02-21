64fba006b9a63f4174615806a5e8ae0614a3fb53
ryuichi-works <ryu1.works@gmail.com>
2024-02-21 16:21:32 +0000

    modify:#131:seederを本番用データで修正する以前の状態を記録
    以前までのアプリが正常に動いていた状態を記録しておくため

--- database/seeders/GutReviewSeeder.php
+++ database/seeders/GutReviewSeeder.php
@@ -47,6 +47,186 @@ class GutReviewSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
+            [
+                'user_id' => 1,
+                'equipment_id' => 1,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 2,
+                'equipment_id' => 2,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 3,
+                'equipment_id' => 3,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 1,
+                'equipment_id' => 1,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 2,
+                'equipment_id' => 2,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 3,
+                'equipment_id' => 3,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 1,
+                'equipment_id' => 1,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 2,
+                'equipment_id' => 2,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 3,
+                'equipment_id' => 3,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 1,
+                'equipment_id' => 1,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 2,
+                'equipment_id' => 2,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 3,
+                'equipment_id' => 3,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 1,
+                'equipment_id' => 1,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 2,
+                'equipment_id' => 2,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 3,
+                'equipment_id' => 3,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 1,
+                'equipment_id' => 1,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 2,
+                'equipment_id' => 2,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'user_id' => 3,
+                'equipment_id' => 3,
+                'match_rate' => 4.5,
+                'pysical_durability' => 2.5,
+                'performance_durability' => 1.5,
+                'review' => 'すごくいいストリングだと思います',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
         ]);
     }
 }
--- database/seeders/GutSeeder.php
+++ database/seeders/GutSeeder.php
@@ -44,6 +44,168 @@ class GutSeeder extends Seeder
                 'created_at' => Carbon::now(),
                 'updated_at' => Carbon::now()
             ],
+            [
+                'name_ja' => 'ポリツアープロ',
+                'name_en' => 'poly tour pro',
+                'maker_id' => 1,
+                'image_id' => 2,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ブラスト',
+                'name_en' => 'brust',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'プロハリケーンツアー',
+                'name_en' => '',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => true,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ポリツアープロ',
+                'name_en' => 'poly tour pro',
+                'maker_id' => 1,
+                'image_id' => 2,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ブラスト',
+                'name_en' => 'brust',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'プロハリケーンツアー',
+                'name_en' => '',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => true,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ポリツアープロ',
+                'name_en' => 'poly tour pro',
+                'maker_id' => 1,
+                'image_id' => 2,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ブラスト',
+                'name_en' => 'brust',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'プロハリケーンツアー',
+                'name_en' => '',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => true,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ポリツアープロ',
+                'name_en' => 'poly tour pro',
+                'maker_id' => 1,
+                'image_id' => 2,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ブラスト',
+                'name_en' => 'brust',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'プロハリケーンツアー',
+                'name_en' => '',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => true,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ポリツアープロ',
+                'name_en' => 'poly tour pro',
+                'maker_id' => 1,
+                'image_id' => 2,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ブラスト',
+                'name_en' => 'brust',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'プロハリケーンツアー',
+                'name_en' => '',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => true,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ポリツアープロ',
+                'name_en' => 'poly tour pro',
+                'maker_id' => 1,
+                'image_id' => 2,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'ブラスト',
+                'name_en' => 'brust',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => false,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name_ja' => 'プロハリケーンツアー',
+                'name_en' => '',
+                'maker_id' => 2,
+                'image_id' => 1,
+                'need_posting_image' => true,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
         ]);
     }
 }
